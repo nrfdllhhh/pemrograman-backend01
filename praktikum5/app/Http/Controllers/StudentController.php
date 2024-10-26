e66bd8d980c7246564a4c248e87c5d022fd10812
Nur Fadilah <146068607+nrfdllhhh@users.noreply.github.com>
2024-10-26 19:58:53 +0700

    tugas 5 lanjutan

---
+++ praktikum5/app/Http/Controllers/StudentController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+   
+    
+    public function index()
+    {
+        $student = Student::all();
+
+        $data = [
+            'message' => 'Get all student',
+            'data' => $student
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function store(request $request) {
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan,
+
+        ];
+        $student = Student::create($input);
+
+        $data = [
+            'message'=> 'Get all student',
+            'data' => $student
+        ];
+        return response()->json($data,201);
+    }
+
+    public function update(request $request, string $id){
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
+
+        $student->update($request->all());
+
+        $data = [
+            'message' => 'Student updated successfully',
+            'data' => $student
+        ];
+        return response()->json($data, 200);
+        
+    }
+    public function destroy(string $id){
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
+
+        $student->delete();
+
+        return response()->json(['message' => 'Student deleted successfully'], 200);
+    }
+
+    }
+
